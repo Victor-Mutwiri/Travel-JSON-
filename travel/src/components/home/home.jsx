0677ded8de4424cf2716c20da5997f0797390042
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-01-30 14:42:17 +0300

    1.3

--- travel/src/components/home/home.jsx
+++ travel/src/components/home/home.jsx
@@ -1,8 +1,10 @@
+import HomeSlider from '../carousel/homecarousel'
 import './home.css'
 
 export default function Home(){
     return(
         <section className="home">
+            <HomeSlider/>
             <h2>Welcome to Salama Adventures</h2>
             <h3>Here to simplify and help you explore theKenyan Backyard</h3>
             <h4>Travel Safe|Travel Salama</h4>
