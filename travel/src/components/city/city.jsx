b1ab9cbcb5b6d2e6abbc4a8d4cee390d86493a99
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-01-31 22:37:47 +0300

    1.8

--- travel/src/components/city/city.jsx
+++ travel/src/components/city/city.jsx
@@ -1,12 +1,21 @@
 import './city.css'
+import data from '../../destinations.json'
 
 export default function City(){
     return(
         <div className="citypage">
             <h1>Explore Fun & Cool Spots <br/>Around the City</h1>
-            <div className="destinations">
-                
-            </div>
+            {data && data.map(city=>{
+                return(
+                    <div className="city" key={city.id}>
+                        <img src={city.image}/>
+                        <h2><strong>{city.title}</strong></h2>
+                        <p><strong>About:</strong>{city.description}</p>
+                        <p><strong>Location:</strong>{city.location}</p>
+                        <p>ksh{city.prices}/=</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
