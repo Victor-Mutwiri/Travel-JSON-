d9915df2d50c32bc54f7487e9bd8cd95c72a3029
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-02-01 23:56:44 +0300

    1.92

--- travel/src/components/packages/packages.jsx
+++ travel/src/components/packages/packages.jsx
@@ -1,7 +1,24 @@
+import packages from '../../packages.json'
+
 export default function Packages(){
     return (
-        <div>
-            <h2>Awesome Packages Just for you!</h2>
+        <div className="packages">
+            <h2>Explore our Excellent Travel Packages!</h2>
+            <div className="pack">
+                {packages.map(places=>{
+                    return(
+                        <div className="area" key={places.id}>
+                            <img src={places.image} />
+                            <div className='desc'>
+                                <h2><strong>{places.title}</strong></h2>
+                                <p><strong>Major Attractions: </strong>{places.description}</p>
+                                <p><strong>Days: </strong>{places.days}</p>
+                                <p><strong>ksh </strong>{places.prices}/=</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
